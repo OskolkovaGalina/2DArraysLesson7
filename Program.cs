618229d0e0d5c72b9caa5ae848c100fe29b34e2a
galina_oskolkova <galidos@mail.ru>
2023-02-02 15:08:46 +0300

    Задача 50 решена

--- Program.cs
+++ Program.cs
@@ -5,26 +5,79 @@
 //1 -3,3 8 -9,9
 //8 7,8 -7,1 9
 
-int rows = ReadInt("Введите количество строк: ");
-int colums = ReadInt("Введите количество столбцов: ");
-double[,] numbers = new double[rows, colums];
+//int rows = ReadInt("Введите количество строк: ");
+//int colums = ReadInt("Введите количество столбцов: ");
+//double[,] numbers = new double[rows, colums];
+//FillArray2D(numbers);
+//PrintArray2D(numbers);
+
+// Заполнение массива рандомными вещественными числами
+/void FillArray2D(double[,] array)
+//{
+  //  for (int i = 0; i < array.GetLength(0); i++)
+    //{
+      //  for (int j = 0; j < array.GetLength(1); j++)
+        //{
+          //  array[i, j] = new Random().Next(-99, 99) / 10.0;
+        //}
+    //}
+//}
+
+//  Функция вывода массива в терминал
+//void PrintArray2D(double[,] array)
+//{
+  //  for (int i = 0; i < array.GetLength(0); i++)
+   // {
+     //   for (int j = 0; j < array.GetLength(1); j++)
+       // {
+         //   Console.Write(array[i, j] + " ");
+        //}
+        //Console.WriteLine();
+    //}
+    //Console.WriteLine();
+//}
+
+// Функция ввода 
+//int ReadInt(string message)
+//{
+  //  Console.Write(message);
+    //return Convert.ToInt32(Console.ReadLine());
+//}
+
+
+//Задача 50. Напишите программу, которая на вход принимает значение элемента 
+//в двумерном массиве, и возвращает позицию этого элемента или же указание, 
+//что такого элемента нет.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//17 -> такого числа в массиве нет
+
+int rows = ReadInt("Введите индекс строки: ");
+int colums = ReadInt("Введите индекс столбца: ");
+int[,] numbers = new int[6, 8];
 FillArray2D(numbers);
 PrintArray2D(numbers);
 
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(double[,] array)
+if (rows < numbers.GetLength(0) && colums < numbers.GetLength(1)) Console.WriteLine(numbers[rows, colums]);
+else Console.WriteLine($"{rows}{colums} -> такого числа в массиве нет");
+
+
+// Заполнение массива рандомными числами от 1 до 9
+void FillArray2D(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = new Random().Next(-99, 99) / 10.0;
+            array[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
 //  Функция вывода массива в терминал
-void PrintArray2D(double[,] array)
+void PrintArray2D(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
