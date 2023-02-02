df13560b177c871e8d43f6fcab51f5ec77383ecd
galina_oskolkova <galidos@mail.ru>
2023-02-02 16:02:59 +0300

    Задача 52 решена

--- Program.cs
+++ Program.cs
@@ -91,68 +91,26 @@
 //8 4 2 4
 //Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
 
-int rows = ReadInt("Введите номер строки: ");
-int colums = ReadInt("Введите номер столбца: ");
-
-int rows = 3;
-int colums = 4;
-int[,] numbers = new int[rows, colums];
-FillArray2D(numbers);
-PrintArray2D(numbers);
-
-double[] avgNumbers = new double[numbers.GetLength(1)];
-
-for (int i = 0; i < numbers.GetLength(1); i++)
+Random random = new Random();
+int[,] arr = new int[random.Next(1, 10), random.Next(1, 10)];
+for (int i = 0; i < arr.GetLength(0); i++)
 {
-    double result = 0.0;
-    for (int j = 0; j < numbers.GetLength(0); j++)
+    for (int j = 0; j < arr.GetLength(1); j++)
     {
-        result += numbers[j, i];
-    }
-    avgNumbers[i] = result / numbers.GetLength(0);
-}
-PrintArray(avgNumbers);
-
-
-// Заполнение массива рандомными вещественными числами
-void FillArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(1, 10);
-        }
-    }
-}
-
-//  Функция вывода двумерного массива в терминал
-void PrintArray2D(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.WriteLine();
+        arr[i, j] = random.Next(1, 10);
+        Console.Write(arr[i, j] + " ");
     }
     Console.WriteLine();
 }
-
-// Функция вывода массива в терминал 
-void PrintArray(double[] array)
+Console.WriteLine("---------------------------");
+Console.WriteLine(arr.GetLength(0));
+for (int j = 0; j < arr.GetLength(1); j++)
 {
-    for (int i = 0; i < array.Length; i++)
+    double sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
     {
-        Console.Write(array[i] + " ");
+        sum += arr[i, j];
     }
-    Console.WriteLine();
+    Console.Write($"{ sum / arr.GetLength(0)} ");
 }
-
-// Функция ввода 
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
+Console.ReadLine();
