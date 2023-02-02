17210338d68378e6b87edf44827ac5e33c3b884c
galina_oskolkova <galidos@mail.ru>
2023-02-02 16:00:14 +0300

    Задача 52 решена

--- Program.cs
+++ Program.cs
@@ -54,30 +54,105 @@
 //8 4 2 4
 //17 -> такого числа в массиве нет
 
-Console.Write("Введите строку: ");
-int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
-Console.Write("Введите столбец: ");
-int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
-int n = 5; 
-int m = 7; 
-Random random = new Random();
-int[,] arr = new int[n, m];
-Console.WriteLine("Исходный массив: ");
-for (int i = 0; i < arr.GetLength(0); i++)
+//Console.Write("Введите строку: ");
+//int pos1 = Convert.ToInt32(Console.ReadLine()) - 1;
+//Console.Write("Введите столбец: ");
+//int pos2 = Convert.ToInt32(Console.ReadLine()) - 1;
+//int n = 5; 
+//int m = 7; 
+//Random random = new Random();
+//int[,] arr = new int[n, m];
+//Console.WriteLine("Исходный массив: ");
+//for (int i = 0; i < arr.GetLength(0); i++)
+//{
+  //  for (int j = 0; j < arr.GetLength(1); j++)
+//{
+  //  arr[i, j] = random.Next(10, 99);
+//Console.Write("{0} ", arr[i, j]);
+//}
+//Console.WriteLine();
+//}
+  //  if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+//{
+//Console.WriteLine("Элемент не существует  ");
+//}
+  //  else
+//{
+  //  Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+//}
+//Console.ReadLine();
+
+
+//Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+//арифметическое элементов в каждом столбце.
+//Например, задан массив:
+//1 4 7 2
+//5 9 2 3
+//8 4 2 4
+//Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+// int rows = ReadInt("Введите номер строки: ");
+// int colums = ReadInt("Введите номер столбца: ");
+
+int rows = 3;
+int colums = 4;
+int[,] numbers = new int[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
 {
-    for (int j = 0; j < arr.GetLength(1); j++)
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArray(avgNumbers);
+
+
+// Заполнение массива рандомными вещественными числами
+void FillArray2D(int[,] array)
 {
-    arr[i, j] = random.Next(10, 99);
-Console.Write("{0} ", arr[i, j]);
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
 }
-Console.WriteLine();
-}
-    if (pos1 < 0 | pos1 > arr.GetLength(0) - 1 | pos2 < 0 | pos2 > arr.GetLength(1) - 1)
+
+//  Функция вывода двумерного массива в терминал
+void PrintArray2D(int[,] array)
 {
-Console.WriteLine("Элемент не существует  ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
 }
-    else
+
+// Функция вывода массива в терминал 
+void PrintArray(double[] array)
 {
-    Console.WriteLine("Значение элемента массива = {0}", arr[pos1, pos2]);
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
 }
-Console.ReadLine();
+
+// Функция ввода 
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
